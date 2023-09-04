1a2439b8dfb7424a42bab7e9acaa16062b711e6a
unknown <waldoch.don@gmail.com>
2023-09-04 15:31:16 -0500

    Initialize Express App

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+require('dotenv').config()
+const express = require('express')
+
+// Configuration
+const app = express()
+
+// Middleware
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the VCD Worship Synch App!')
+})
+
+app.use(function (req, res, next) {
+	res.status(404).send('<h1> Page not found </h1>');
+});
+
+// Listen
+app.listen(process.env.PORT, () => {
+  console.log('Connected to Server Port:', process.env.PORT);
+})
